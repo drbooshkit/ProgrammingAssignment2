81811dbd5b259a7f49a785f54e5be7fa1f86f711
Brian Zias <bczias@gmail.com>
2014-04-28 19:25:55 -0700

    submit for assignment 2 - peer assesment
    initial submit with raw outline of code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## take advantage of the scoping rules of the R language by
+## preserving the inverse of an invertable matrix
 
-## Write a short comment describing this function
+
+## makeCacheMatrix creates a "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  makeVector <- function(x = numeric()) {
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setmean <- function(mean) m <<- mean
+    getmean <- function() m
+    list(set = set, get = get,
+         setmean = setmean,
+         getmean = getmean)
+  }
 }
 
 
-## Write a short comment describing this function
+## computes the inverse of the "matrix" returned by `makeCacheMatrix` above
+## If the inverse has already been calculated (and the matrix has not changed),
+## then `cacheSolve` should retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+    m <- x$getmean()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- mean(data, ...)
+    x$setmean(m)
+    m
+  }
 }
